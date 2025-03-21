9fed188f8e68332340d2556ff8dd5c4bd93b453b
orukdoline <skynets@kakao.com>
2025-03-21 14:07:21 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -6,30 +6,17 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        class Person {
-            private final int height;
-            private final int weight;
-            public Person(int height, int weight) {
-                this.height = height;
-                this.weight = weight;
-            }
-            public int getHeight() {
-                return height;
-            }
-            public int getWeight() {
-                return weight;
-            }
-        }
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        Person[] persons = new Person[N];
+        int[] height = new int[N];
+        int[] weight = new int[N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Person person = new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            persons[i] = person;
+            height[i] = Integer.parseInt(st.nextToken());
+            weight[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] rank = new int[N];
@@ -37,7 +24,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (i == j) continue;
-                if (persons[i].getHeight() < persons[j].getHeight() && persons[i].getWeight() < persons[j].getWeight()) {
+                if (height[i] < height[j] && weight[i] < weight[j]) {
                     result++;
                 }
             }
