479114dc4d4ad27d09c7b2c3fb888157fa6a3a6a
orukdoline <skynets@kakao.com>
2025-03-25 13:36:51 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("강한친구 대한육군\n" + "강한친구 대한육군\n");
+        bw.flush();
+    }
+}
