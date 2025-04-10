2d9f97ab1c3560de054781d99269af8cdd1ef7d6
orukdoline <skynets@kakao.com>
2025-04-10 15:51:01 +0900

    [Bronze V] Title: Shares, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while((input = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(input);
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            bw.write(M / (N + 1) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
