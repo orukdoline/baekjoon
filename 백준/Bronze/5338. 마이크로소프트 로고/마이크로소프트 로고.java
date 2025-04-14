767c12cdd31ff528e411d993466257e5c40eb8d5
orukdoline <skynets@kakao.com>
2025-04-14 12:02:12 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
