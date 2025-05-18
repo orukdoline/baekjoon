ebb219cd05e3cc8d95deba9133ae3600dd7a2e00
orukdoline <skynets@kakao.com>
2025-05-18 13:46:33 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%-15s", "SHIP NAME");
+        System.out.printf("%-15s", "CLASS");
+        System.out.printf("%-11s", "DEPLOYMENT");
+        System.out.printf("%-10s\n", "IN SERVICE");
+
+        System.out.printf("%-15s", "N2 Bomber");
+        System.out.printf("%-15s", "Heavy Fighter");
+        System.out.printf("%-11s", "Limited");
+        System.out.printf("%-10s\n", "21");
+
+        System.out.printf("%-15s", "J-Type 327");
+        System.out.printf("%-15s", "Light Combat");
+        System.out.printf("%-11s", "Unlimited");
+        System.out.printf("%-10s\n", "1");
+
+        System.out.printf("%-15s", "NX Cruiser");
+        System.out.printf("%-15s", "Medium Fighter");
+        System.out.printf("%-11s", "Limited");
+        System.out.printf("%-10s\n", "18");
+
+        System.out.printf("%-15s", "N1 Starfighter");
+        System.out.printf("%-15s", "Medium Fighter");
+        System.out.printf("%-11s", "Unlimited");
+        System.out.printf("%-10s\n", "25");
+
+        System.out.printf("%-15s", "Royal Cruiser");
+        System.out.printf("%-15s", "Light Combat");
+        System.out.printf("%-11s", "Limited");
+        System.out.printf("%-10s\n", "4");
+    }
+}
